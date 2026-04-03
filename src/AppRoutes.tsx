8e81ae0605b111baea41a5f8b166b35d2e41304f
Vladyslav Aheiev <vageev06@gmail.com>
2026-04-02 22:02:35 -0400

    feat: add 404 page

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import { HomePage } from "./pages/HomePage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 export function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,8 @@
+export function NotFoundPage() {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>The page you're looking for is not found</p>
+    </div>
+  );
+}
