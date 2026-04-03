f092a764a26e2301ed2efdc9542934af33d78598
Vladyslav Aheiev <vageev06@gmail.com>
2026-04-02 21:57:30 -0400

    feat: integrate AppRoutes with main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import { BrowserRouter } from "react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { AppRoutes } from "./AppRoutes.tsx";
 import "./index.css";
-import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AppRoutes />
   </StrictMode>,
 );
